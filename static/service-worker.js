cde9227810a3ec815bcc42c7f913a9e653a18462
dolcetriade <vcelestialragev@gmail.com>
2019-11-27 04:58:20 -0800

    Update frontend with chat

--- static/service-worker.js
+++ static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b7c2d62138975d503f19591025fc357.js"
+  "/precache-manifest.814243678a620b55de674715bac06bc9.js"
 );
 
 self.addEventListener('message', (event) => {
